82a39fa1472e66098f72742a83f4de3c6da8fceb
davidbrassimoes <david.simoes.1994@gmail.com>
2022-02-16 22:27:34 +0000

    Fim Aula React 16 fev

--- voting-app/src/components/Product.jsx
+++ voting-app/src/components/Product.jsx
@@ -1,20 +1,45 @@
 import React, { Component } from 'react'
 
 export class Product extends Component {
+
+    handleUpVote = () => {
+        // this.props.onVote(this.props.id);
+
+        const { onVote, id } = this.props;
+        onVote(id);
+    }
+    handleDownVote = () => {
+        // this.props.onVote(this.props.id);
+
+        const { onUnvote, id } = this.props;
+        onUnvote(id);
+    }
+
+
     render() {
+        const { id, title, description, url, votes, submitterAvatarUrl, productImageUrl } = this.props
         return (
             <div className='item'>
                 <div className='image'>
-                    <img src={this.props.productImageUrl} />
+                    <img src={productImageUrl} />
                 </div>
                 <div className='middle aligned content'>
+                    <div className='header'>
+                        <a onClick={this.handleUpVote}>
+                            <i className='large caret up icon' />
+                        </a>
+                        <a onClick={this.handleDownVote}>
+                            <i className='large caret down icon' />
+                        </a>
+                        {votes}
+                    </div>
                     <div className='description'>
-                        <a>{this.props.title} </a>
-                        <p> {this.props.description} </p>
+                        <a>{title} </a>
+                        <p> {description} </p>
                     </div>
                     <div className='extra'>
                         <span>Submitted by: </span>
-                        <img className='ui avatar image' src={this.props.submitterAvatarUrl} />
+                        <img className='ui avatar image' src={submitterAvatarUrl} />
                     </div>
                 </div>
             </div>
--- voting-app/src/components/ProductList.jsx
+++ voting-app/src/components/ProductList.jsx
@@ -5,22 +5,101 @@ import Product from './Product'
 import { products } from '../seeds'
 
 export class ProductList extends Component {
+
+    state = {
+        products: [],
+        descending: true,
+    }
+
+    toggleSort = () => {
+        this.setState({
+            descending: !this.state.descending
+        })
+    }
+
+    handleProductUpVote = (productId) => {
+        console.log(`Foi votado o produto com id: ${productId}`);
+
+        const nextProducts = this.state.products.map(
+            p => {
+                if (p.id === productId) {
+                    return { ...p, votes: p.votes + 1 }
+                } else {
+                    return p;
+                }
+            }
+        )
+
+        this.setState({ products: nextProducts })
+
+        /*     products.forEach((p) => {
+                if (p.id === productId) {
+                    p.votes += 1;
+                }
+            }) */
+
+    }
+    handleProductDownVote = (productId) => {
+        console.log(`Foi votado o produto com id: ${productId}`);
+
+        const nextProducts = this.state.products.map(
+            p => {
+                if (p.id === productId) {
+                    return { ...p, votes: p.votes - 1 }
+                } else {
+                    return p;
+                }
+            }
+        )
+
+        this.setState({ products: nextProducts })
+
+        /*     products.forEach((p) => {
+                if (p.id === productId) {
+                    p.votes += 1;
+                }
+            }) */
+
+    }
+
+    componentDidMount() {
+        this.setState({ products })
+    }
+
+
     render() {
 
-        const product = products[0];
-        console.log(product);
+        const sortedProducts = this.state.products.sort((a, b) => this.state.descending ? (b.votes - a.votes) : (a.votes - b.votes))
+
+        const productComponents = sortedProducts.map((p, index) => (
+
+            <Product
+                key2={index}
+                key={'productID-' + p.id}
+                id={p.id}
+                title={p.title}
+                description={p.description}
+                url={p.url}
+                votes={p.votes}
+                submitterAvatarUrl={p.submitterAvatarUrl}
+                productImageUrl={p.productImageUrl}
+
+                onVote={this.handleProductUpVote}
+                onUnvote={this.handleProductDownVote}
+            />
+        ))
+
+        // const { id, title, description, url, votes, submitterAvatarUrl, productImageUrl } = products[0];
+        // const product = products[0];
+        // console.log(product);
+
 
         return (
             <div className='ui unstackable items'>
-                <Product
-                    id={product.id}
-                    title={product.title}
-                    description={product.description}
-                    url={product.url}
-                    votes={product.votes}
-                    submitterAvatarUrl={product.submitterAvatarUrl}
-                    productImageUrl={product.productImageUrl}
-                />
+                <button onClick={this.toggleSort} >
+                    up / down
+                </button>
+                {productComponents}
             </div>
         )
     }
