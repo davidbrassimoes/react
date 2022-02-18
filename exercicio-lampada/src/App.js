4d814ac92674307207f29f51fdf4ffe699c17100
davidbrassimoes <david.simoes.1994@gmail.com>
2022-02-18 22:48:57 +0000

    Please enter the commit message for your changes. Lines starting

---
+++ exercicio-lampada/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import LightbulbSvg from './Lampada';
+
+function App() {
+  return (
+    <div className="App">
+      <LightbulbSvg fillColor={fillColor} />
+    </div>
+  );
+}
+
+export default App;
