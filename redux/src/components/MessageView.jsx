7afaef032bccb677fe3b113e99ef82a6785ecf64
davidbrassimoes <david.simoes.1994@gmail.com>
2022-03-02 23:01:54 +0000

    message view

--- redux/src/components/MessageView.jsx
+++ redux/src/components/MessageView.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { bindActionCreators } from 'redux';
 
 export default function MessageView({ messages }) {
+
+    let dispatch = useDispatch();
+
+    const handleDelete = (index) => {
+        dispatch({
+            type: "DELETE_MESSAGE",
+            index,
+        })
+    }
     return (
         <div>
             {
                 messages.map((m, i) => (
-                    <h4 key={i} > {m} </h4>
+                    <h4 key={i} onClick={handleDelete} > {m} </h4>
                 ))
             }
         </div>
